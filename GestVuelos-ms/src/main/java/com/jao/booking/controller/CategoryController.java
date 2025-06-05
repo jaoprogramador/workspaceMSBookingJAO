d2ec89183a882f0b420bae340b150d807391f305
jao <jaoprogramador@gmail.com>
2025-06-05 11:46:10 +0200

    version III

--- GestVuelos-ms/src/main/java/com/jao/booking/controller/CategoryController.java
+++ GestVuelos-ms/src/main/java/com/jao/booking/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.jao.booking.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @RequestMapping("api/categories")
 @Getter
 @Setter
+@RefreshScope
 public class CategoryController {
 	
 	@Value("${app.testProp}")
