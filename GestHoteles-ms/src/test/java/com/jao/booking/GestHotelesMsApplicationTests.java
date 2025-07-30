a5c11bb2710f7feb94ed4cd6f247979893c90cbb
jao <jaoprogramador@gmail.com>
2025-07-30 21:41:30 +0200

    config server datasoruce para usuarios y vuelos

--- GestHoteles-ms/src/test/java/com/jao/booking/GestHotelesMsApplicationTests.java
+++ GestHoteles-ms/src/test/java/com/jao/booking/GestHotelesMsApplicationTests.java
@@ -3,11 +3,11 @@ package com.jao.booking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class GestHotelesMsApplicationTests {
 
-	//@Test
-	//void contextLoads() {
-	//}
+	@Test
+	void contextLoads() {
+	}
 
 }
